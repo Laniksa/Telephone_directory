0cc199ed8f5e5c8d99b318c11c0a4d14f0accc97
Регина <rmashkovtseva@gmail.com>
2023-05-02 21:00:00 +0300

    набросок

---
+++ src/com/company/Contact.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Contact {
+private String name;
+private int number;
+
+public Contact(String name, int number){
+    this.name = name;
+    this.number = number;
+}
+}
---
+++ src/com/company/PhoneBook.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class PhoneBook {
+    private Contact contact;
+    private String group;
+
+    public  PhoneBook(Contact contact, String group){
+        this.contact = contact;
+        this.group = group;
+    }
+}
