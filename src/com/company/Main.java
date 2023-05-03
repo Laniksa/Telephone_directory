5ff282c6060c62f93c5db62067eca9414a204c45
Регина <rmashkovtseva@gmail.com>
2023-05-03 10:58:24 +0300

    Запись в два метода

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,40 @@ import java.util.Scanner;
 public class Main {
     public static List<Contact> contacts = new ArrayList<>();
     public static Scanner scan;
-public static void record (){
-    scan = new Scanner(System.in);
-    System.out.println("Введите название группы");
-    String group = scan.nextLine();
 
-
-        System.out.println("Введите имя контакта");
-        String name = scan.nextLine();
-        System.out.println("Введите номер контакта");
-        int number = scan.nextInt();
-
-        contacts.add(new Contact(name, number));
-
+    public static void recordGroup(List<Contact> contacts) {
+        scan = new Scanner(System.in);
+        System.out.println("Введите название группы");
+        String group = scan.nextLine();
 
         PhoneBook.note.put(group, contacts);
 
-}
+    }
+
+    public static boolean recordContact() {
+        while (true) {
+            System.out.println("Введите имя контакта");
+            String name = scan.nextLine();
+            System.out.println("Введите номер контакта");
+            int number = scan.nextInt();
+
+            contacts.add(new Contact(name, number));
+            System.out.println("Добавить еще один контакт?\n" +
+                    "1.Да" +
+                    "2.Нет");
+            int num = scan.nextInt();
+            if(num == 1){
+                return true;
+            }else{
+                System.out.println("Запись контактов завершена");
+                return false;
+            }
+        }
+
+    }
 
 
     public static void main(String[] args) {
-	record();
+        //record();
     }
 }
