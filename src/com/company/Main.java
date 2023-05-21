9fdfffd3879e9039c179e780fbc59a7db00d1d95
Регина <rmashkovtseva@gmail.com>
2023-05-21 21:44:14 +0300

    Методы перемещены в класс phonebook/ нО ДОБАВЛЕНИЕ НЕ РАБОТАЕТ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,43 +5,11 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.*;
 
 public class Main {
-    public static ArrayList<Contact> contacts = new ArrayList<>();
-    public static Scanner scan;
-
-    public static void recordGroup() {
-        scan = new Scanner(System.in);
-        System.out.println("Введите название группы");
-        String group = scan.nextLine();
-        addPhoneBook(group, recordContact());
-    }
-
-    public static ArrayList<Contact> recordContact() {
-
-        int num = 0;
-        while (num !=2) {
-            scan = new Scanner(System.in);
-            System.out.println("Введите имя контакта");
-            String name = scan.nextLine();
-
-            System.out.println("Введите номер контакта");
-            int number = scan.nextInt();
-
-            contacts.add(new Contact(name, number));
-            System.out.println(contacts.toString() + " записан.\n Добавить еще один контакт? \n 1.Да\n 2.Нет");
-            num = scan.nextInt();
-        }
-        System.out.println("Запись контактов завершена!");
-
-        return contacts;
 
 
-    }
 
-    public static void addPhoneBook(String group, ArrayList<Contact> contacts){
-        //PhoneBook phoneBook = new PhoneBook (group, (ArrayList<Contact>) contacts);
-        PhoneBook.note.put(group,contacts);
-        printContactsGroups();
-    }
+
+
 
 
 //    public static boolean addContact() {
@@ -76,10 +44,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        recordGroup();
-        //conclusionContact();
-       // printContactsGroups();
-//        recordContact();
+
+        PhoneBook.recordGroup();
 
 
 
--- src/com/company/PhoneBook.java
+++ src/com/company/PhoneBook.java
@@ -2,15 +2,54 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class PhoneBook {
     public static HashMap<String, ArrayList<Contact>> note;
+    public static ArrayList<Contact> contacts = new ArrayList<>();
+    public static Scanner scan;
 
-    public PhoneBook(String group, ArrayList<Contact> contact){
+    public PhoneBook(String group, ArrayList<Contact> contacts){
         note = new HashMap<>();
-        note.put(group, contact);
+        note.put(group, contacts);
     }
+
+    public static void recordGroup() {
+        int num = 0;
+        while (num != 2){
+            scan = new Scanner(System.in);
+            System.out.println("Введите название группы");
+            String group = scan.nextLine();
+            note.put(group, recordContact());//почему-то не работает
+            System.out.println("Добавить еще одну группу с контактами?\n 1.Да \n 2.Нет");
+        }
+        //PhoneBook.note.toString();
+
+    }
+
+    public static ArrayList<Contact> recordContact() {
+
+        int num = 0;
+        while (num !=2) {
+            scan = new Scanner(System.in);
+            System.out.println("Введите имя контакта");
+            String name = scan.nextLine();
+
+            System.out.println("Введите номер контакта");
+            int number = scan.nextInt();
+
+            contacts.add(new Contact(name, number));
+            System.out.println(contacts.toString() + " записан.\n Добавить еще один контакт? \n 1.Да\n 2.Нет");
+            num = scan.nextInt();
+        }
+        System.out.println("Запись контактов завершена!");
+
+        return contacts;
+
+
+    }
+
 
     @Override
     public String toString() {
